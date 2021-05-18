454030964f5d425c337b6989b5c916602c37723a
Zawisz <zawisza_mateusz@wp.pl>
2021-05-18 21:07:27 +0200

    Fast expedition scene setup

---
+++ Assets/Scripts/ExpeditionMap/FastMapGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FastMapGenerator : MonoBehaviour
+{
+    public GameObject root = null;
+    public GameObject fieldPrefab = null;
+    public Vector2 mapDimentions = Vector2.zero;
+
+    public bool execute = false;
+
+    private void Update()
+    {
+        if (!execute)
+            return;
+
+        execute = !execute;
+
+        for (int x = 0; x < mapDimentions.x; x++)
+        {
+            for (int z = 0; z < mapDimentions.y; z++)
+            {
+                GameObject fieldGO = Instantiate(fieldPrefab, this.transform.position + new Vector3(x, 0, z), Quaternion.identity, root.transform);
+                fieldGO.name = "Field (" + x + ", " + z + ")";
+            }
+        }
+    }
+}
