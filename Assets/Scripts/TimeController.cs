61464bae84a607c2ca2085d59bb7d8957651b5c4
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-05-18 22:22:51 +0200

    Adding Days UI
    Importing TMPro

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimeController : MonoBehaviour
 {
     public static TimeController Instance = null;
 
+    [SerializeField] private TextMeshProUGUI daysText;
+    [SerializeField] private TextMeshProUGUI monthsText;
+    [SerializeField] private TextMeshProUGUI currentDayText;
+
     private DayType currentDay;
-    private int totalDays = 0;
+    private int totalDays = 1;
+    private int thisMonthDays = 1;
     private int months = 0;
 
     public int TotalDays { get => totalDays; }
@@ -22,14 +28,23 @@ public class TimeController : MonoBehaviour
     
     void Start()
     {
-        totalDays = 0;
+        totalDays = 1;
         months = 0;
-        currentDay = DayType.Sunday;
+        currentDay = DayType.Niedziela;
+        UpdateUI();
+    }
+
+    public void SkipToSunday()
+    {
+        while(currentDay != DayType.Niedziela)
+        {
+            NextDay();
+        }
     }
 
     public void NextDay()
     {
-        if (CurrentDay != DayType.Sunday)
+        if (CurrentDay != DayType.Niedziela)
         {
             currentDay ++;
             
@@ -37,24 +52,43 @@ public class TimeController : MonoBehaviour
         }
         else
         {
-            currentDay = DayType.Monday;
+            currentDay = DayType.Poniedziałek;
 
             // FAZA BUDOWANIA
         }
 
         totalDays ++;
-        if (totalDays % 30 == 0)
+        thisMonthDays++;
+        if (thisMonthDays % 30 == 0)
+        {
+            thisMonthDays = 0;
             months++;
+        }
+
+        UpdateUI();
     }
 
+    public void UpdateUI()
+    {
+        if(currentDayText)
+            currentDayText.text = currentDay.ToString();
+
+        if(daysText)
+            daysText.text = thisMonthDays.ToString();
+
+        if(monthsText)
+            monthsText.text = months.ToString();
+    }
+
+    // Polish signs in code ;_;
     public enum DayType
     {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
+        Poniedziałek,
+        Wtorek,
+        Środa,
+        Czwartek,
+        Piątek,
+        Sobota,
+        Niedziela
     }
 }
