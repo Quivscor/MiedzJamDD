2fccdbce027cff3707d1fb61782b1a57d1399f56
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-05-20 14:45:54 +0200

    Futuristic UI for Buildings Description

---
+++ Assets/Scripts/BuildingDescriptionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildingDescriptionController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI buildingNameText;
+    [SerializeField] private TextMeshProUGUI categoryText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private TextMeshProUGUI bonusesText;
+
+    private void Start()
+    {
+        foreach(Building building in FindObjectsOfType<Building>())
+        {
+            building.OnClick += UpdateUI;
+        }
+
+    }
+
+    public void UpdateUI()
+    {
+        buildingNameText.text = "Energol";
+        categoryText.text = "Energetyka";
+        descriptionText.text = "Fajnie świeci i daje dużo prądu";
+        bonusesText.text = "- Bonus +1/n-Bonus +2";
+    }
+}
