3eabce5aa3da823734d30ad5415a5516da6f819d
Zawisz <zawisza_mateusz@wp.pl>
2021-05-20 19:10:56 +0200

    Button to switch "scenes"

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject[] citySceneObjects;
+    public GameObject[] expeditionSceneObjects;
+
+    public void SwitchToCityScene()
+    {
+        foreach (GameObject go in citySceneObjects)
+            go.SetActive(true);
+
+        foreach (GameObject go in expeditionSceneObjects)
+            go.SetActive(false);
+    }
+
+    public void SwitchToExpeditionScene()
+    {
+        foreach (GameObject go in citySceneObjects)
+            go.SetActive(false);
+
+        foreach (GameObject go in expeditionSceneObjects)
+            go.SetActive(true);
+    }
+}
