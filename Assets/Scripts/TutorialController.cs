8ba0ca712840295321b63ee64e4feb5a901cbe96
Quivscor <quivscor@gmail.com>
2021-05-21 14:53:15 +0200

    TutorialController WIP

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMProText = TMPro.TextMeshProUGUI;
+
+public class TutorialController : MonoBehaviour
+{
+    public List<TutorialSequence> sequences;
+    private int currentSequenceIndex = 0;
+    private int currentDialogueInSequenceIndex = 0;
+
+    public GameObject TutorialHUD;
+    public TMProText dialogueText;
+    public Image tutorialLadyImage; 
+
+    public void DisplaySequence()
+    {
+        //Here be UI shenanigans
+
+        currentSequenceIndex++;
+    }    
+
+    public void ButtonNext()
+    {
+
+    }
+
+    public void HideDisplay()
+    {
+        currentDialogueInSequenceIndex = 0;
+        //Here be UI shenanigans
+    }
+}
---
+++ Assets/Scripts/TutorialSequence.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TutorialSequence : ScriptableObject
+{
+    public List<Dialogue> dialogues;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public Sprite dialogueSprite;
+    [TextArea(1,4)]
+    public string dialogueMsg;
+}
