60ae660a69f9548a06da88f7ba66b15b17ea79b9
Quivscor <quivscor@gmail.com>
2021-05-23 18:32:46 +0200

    Esc menu

---
+++ Assets/EscMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscMenu : MonoBehaviour
+{
+    public GameObject HUD;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (HUD.activeInHierarchy)
+                HideMenu();
+            else
+                DisplayMenu();
+        }
+    }
+
+    public void DisplayMenu()
+    {
+        HUD.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        HUD.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
