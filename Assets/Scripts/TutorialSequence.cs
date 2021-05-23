ef17a777f1360d4709c097278a94fbab7f83cad4
Quivscor <quivscor@gmail.com>
2021-05-23 16:15:50 +0200

    Button refs to turn off during tutorial

--- Assets/Scripts/TutorialSequence.cs
+++ Assets/Scripts/TutorialSequence.cs
@@ -9,6 +9,13 @@ public class TutorialSequence
     public string name;
     public List<Dialogue> dialogues;
     public bool hasFired = false;
+
+    public bool doEkspedycji;
+    public bool doMiasta;
+    public bool sklep;
+    public bool paczka;
+    public bool zakonczMiesiac;
+    public bool misjeZZiemi;
 }
 
 [System.Serializable]
