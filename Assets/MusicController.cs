c7d7478948f321d72cd2e764e7d666936ded7160
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-05-23 11:56:25 +0200

    Music controller with 2 clips

---
+++ Assets/MusicController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private AudioClip [] clips;
+
+    private AudioSource audioSource;
+    private int currentMusic = 0;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = clips[currentMusic];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying) 
+        {
+            currentMusic++;
+            if (currentMusic >= clips.Length)
+                currentMusic = 0;
+
+            audioSource.clip = clips[currentMusic];
+            audioSource.Play();
+
+
+        }
+    }
+}
